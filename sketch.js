c4a941dea773b7c3995fc96b54bf7987c606ce68
Tim Bieniosek <tug76662@temple.edu>
2018-05-09 19:34:07 -0400

    rm draw

--- sketch.js
+++ sketch.js
@@ -44,28 +44,28 @@ createCapture(constraints, function(stream) {
 });
 }
 
-function draw() {
-  // image(capture,0,0,width,height);//,240,120,180,240);
-  // loadPixels()
-  // for( x = 0; x < width; x+=pxSz ) {
-  //   for( y = 0; y < height; y+=pxSz ) {
-  //     idx = 4 * ((y * d) * width * d + (x * d ));
-  //     c = color( pixels[idx], pixels[idx+1], pixels[idx+2] )
-  //     //if(x > width / 2 ) {
-  //       b = brightness(c)
-  //       if( b < 20 ) {
-  //         c = palette[0]
-  //       } else if( b < 45 ) {
-  //         c = palette[1]
-  //       } else if( b < 55 ) {
-  //         c = palette[2]
-  //       } else {
-  //         c = palette[3]
-  //       }
-  //   //  }
-  //     //stroke(c)
-  //     fill(c)
-  //     rect( x, y, pxSz, pxSz )
-  //   }
-  // }
-}
+// function draw() {
+//   // image(capture,0,0,width,height);//,240,120,180,240);
+//   // loadPixels()
+//   // for( x = 0; x < width; x+=pxSz ) {
+//   //   for( y = 0; y < height; y+=pxSz ) {
+//   //     idx = 4 * ((y * d) * width * d + (x * d ));
+//   //     c = color( pixels[idx], pixels[idx+1], pixels[idx+2] )
+//   //     //if(x > width / 2 ) {
+//   //       b = brightness(c)
+//   //       if( b < 20 ) {
+//   //         c = palette[0]
+//   //       } else if( b < 45 ) {
+//   //         c = palette[1]
+//   //       } else if( b < 55 ) {
+//   //         c = palette[2]
+//   //       } else {
+//   //         c = palette[3]
+//   //       }
+//   //   //  }
+//   //     //stroke(c)
+//   //     fill(c)
+//   //     rect( x, y, pxSz, pxSz )
+//   //   }
+//   // }
+// }
