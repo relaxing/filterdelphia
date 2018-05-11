f8286000fda9583129cb77140a67bbb8beff5f07
Tim Bieniosek <tug76662@temple.edu>
2018-05-11 16:36:48 -0400

    Hoping this fixes iOS issues.

--- sketch.js
+++ sketch.js
@@ -1,72 +1,48 @@
-var pxSz = 4;
-var w = 240;
-var h = 320;
+var w = 640;
+var h = 480;
 var capture;
+var canvas;
+var d = 1;
+var pxSz = 4;
+var palette = [];
 
 function setup() {
   capture = createCapture({
-      audio: false,
-      video: {
-          width: w,
-          height: h
-      },
-      mandatory: {
-        minWidth: w,
-        minHeight: h
-      },
-      optional: [{ maxFrameRate: 30 }]
-    }, function() {
-        console.log('capture ready.')
-    });
-  capture.elt.setAttribute('autoplay', '');
+    audio: false,
+    video: true
+  });
   capture.elt.setAttribute('playsinline', '');
-  capture.hide()
-  capture.size(w, h);
+  capture.elt.setAttribute('autoplay', '');
+  canvas = createCanvas(w,h);//capture.width,capture.height);
+  //console.log(capture.width + " " + capture.height)
+  capture.hide();
   d = pixelDensity();
-  noStroke();
   palette = [color( 75, 190, 255 ),
              color( 130, 14, 6 ),
              color( 194, 150, 238 ),
              color( 50, 205, 250)]
-  createCanvas( w, h )
-//   createCanvas(320, 180);
-// var constraints = {
-//   video: {
-//     mandatory: {
-//       minWidth: 320,
-//       minHeight: 180
-//     },
-//     optional: [{ maxFrameRate: 10 }]
-//   },
-//   audio: false
-// };
-// createCapture(constraints, function(stream) {
-//   console.log(stream);
-// });
+  noStroke();
 }
 
 function draw() {
-  image(capture,0,0,width,height);//,240,120,180,240);
+  image(capture, 0, 0, width, height);
   loadPixels()
   for( x = 0; x < width; x+=pxSz ) {
     for( y = 0; y < height; y+=pxSz ) {
       idx = 4 * ((y * d) * width * d + (x * d ));
       c = color( pixels[idx], pixels[idx+1], pixels[idx+2] )
-      //if(x > width / 2 ) {
-        b = brightness(c)
-        if( b < 20 ) {
-          c = palette[0]
-        } else if( b < 45 ) {
-          c = palette[1]
-        } else if( b < 55 ) {
-          c = palette[2]
-        } else {
-          c = palette[3]
-        }
-    //  }
-      //stroke(c)
+      b = brightness(c)
+      if( b < 20 ) {
+        c = palette[0]
+      } else if( b < 45 ) {
+        c = palette[1]
+      } else if( b < 55 ) {
+        c = palette[2]
+      } else {
+        c = palette[3]
+      }
       fill(c)
-      rect( x, y, pxSz, pxSz )
+      rect( x, y, random(pxSz*2), random(pxSz*2) )
     }
   }
 }
